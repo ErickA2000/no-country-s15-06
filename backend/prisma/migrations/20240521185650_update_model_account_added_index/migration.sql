89d906ac59a62a78674a2d462ff42469462c61f5
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2024-05-21 14:00:04 -0500

    feat: :construction: created user service

---
+++ backend/prisma/migrations/20240521185650_update_model_account_added_index/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "Account_email_idx" ON "Account"("email");
+
+-- CreateIndex
+CREATE INDEX "Account_username_idx" ON "Account"("username");
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,101 @@
+import { PaginateResponse } from '@Interfaces/global.interface';
+import { PrismaService } from '@Prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany({
+      include: {
+        account: {
+          select: {
+            id: true,
+            email: true,
+            emailVerified: true,
+            username: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        phoneNumber: true,
+        activity: true,
+        role: true,
+      },
+    });
+  }
+
+  async findAllPaginate(
+    page: number,
+    limit: number,
+  ): Promise<PaginateResponse<User>> {
+    if (page <= 0) page = 1;
+    if (limit <= 0) limit = 20;
+
+    const count = await this.prisma.user.count();
+    const offset = (page - 1) * limit;
+    const pages = Math.ceil(count / limit);
+
+    const users = await this.prisma.user.findMany({
+      skip: offset,
+      take: limit,
+      include: {
+        account: {
+          select: {
+            id: true,
+            email: true,
+            emailVerified: true,
+            username: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        phoneNumber: true,
+        activity: true,
+        role: true,
+      },
+    });
+
+    return this.prisma.paginate<User>(users, {
+      page,
+      limit,
+      pages,
+      count,
+      length: users.length,
+    });
+  }
+
+  async findOneById(id: string): Promise<User> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        account: {
+          select: {
+            id: true,
+            email: true,
+            emailVerified: true,
+            username: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        phoneNumber: true,
+        activity: true,
+        role: true,
+      },
+    });
+  }
+
+  async update(id: string, data: any): Promise<User> {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+}
